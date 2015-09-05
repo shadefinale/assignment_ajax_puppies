0cf27492e110439e3b29834fe4e36e43618314ac
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-09-04 20:31:10 -0400

    reading csvs

--- files.js
+++ files.js
@@ -1,10 +1,18 @@
 FILES = (function(){
 
+  var fileCache;
+
   function initializeFileListener(){
     var $fileButton = $("#csvFile");
     $fileButton.unbind('change');
     $fileButton.change(function(evt) {
-      console.log($(this).val());
+      for (var i = 0; i < this.files.length; i++){
+        var reader = new FileReader()
+        var result = reader.readAsText(this.files[i], "UTF-8")
+        reader.onload = (function(){
+          fileCache = reader.result;
+        })
+      }
     });
   }
 
